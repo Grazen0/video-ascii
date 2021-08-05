a52780a4c91b06c5385404fbe93c1961b533b455
ElCholoGamer <josedanielgrayson@gmail.com>
2021-08-05 16:19:00 -0400

    help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,10 +1,24 @@
+import { RUN_COMMAND } from '../constants';
 import Command from '../structures/command';
+import * as commands from './index';
 
 export const help: Command = {
 	name: 'help',
 	description: 'Shows help',
 	usage: '(command)',
 	execute(args) {
-		console.log('Help command');
+		if (args.length === 0) {
+			const list = Object.values(commands).map(cmd => cmd.name);
+			console.log(`Available commands: ${list.join(', ')}`);
+		} else {
+			const command = (commands as Record<string, Command>)[args[0]];
+			if (!command) {
+				console.log('Invalid command selected');
+			} else {
+				console.log(`${command.name} command`.toUpperCase());
+				console.log(command.description);
+				console.log(`Usage: ${RUN_COMMAND} ${command.name} ${command.usage}`);
+			}
+		}
 	},
 };
